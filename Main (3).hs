ae4bfad2595a04cc7c7fc3385e4eb49a51954a7c
matgimon18a-code <matgimon18a@gmail.com>
2025-08-24 20:16:54 -0500

    Main (3).hs

--- Main (3).hs
+++ Main (3).hs
@@ -1,5 +1,11 @@
 -- Funcion: filtrar lista
 -- Proposito: Elimina los elementos de una lista que estan fuera del intervalo dado
+-- Entradas:
+--   - lista: Lista de números (tipo Num a ⟹ [a])
+--   - minimo: Valor mínimo del intervalo (tipo Num a ⟹ a)
+--   - maximo: Valor máximo del intervalo (tipo Num a ⟹ a)
+-- Salidas: Lista con solo los elementos que están dentro del intervalo [minimo, maximo]
+-- Ejemplo: filtrarLista [1, 25, 5, -4] 0 5 = [1, 5]
 filtrarLista :: (Num a, Ord a) => [a] -> a -> a -> [a]
 filtrarLista [] _ _ = [] -- Caso base: lista vacia
 filtrarLista (x:xs) minimo maximo -- Caso recursivo: x es cabeza, xs es cola
@@ -8,7 +14,10 @@ filtrarLista (x:xs) minimo maximo -- Caso recursivo: x es cabeza, xs es cola
 
 --Funcion:ordenar lista
 --Proposito: Ordena una lista de numeros float en orden descendente(de mayor a menor.
-
+-- Entradas:
+--   - lista: Lista de números de tipo Float ([Float])
+-- Salidas: Lista ordenada en orden descendente
+-- Ejemplo: ordenarLista [1, 25, 5, -4] = [25.0, 5.0, 1.0, -4.0]
 ordenarLista :: [Float] -> [Float]
 ordenarLista [] = [] -- caso base: lista vacia
 ordenarLista [x] = [x] -- caso base: un solo elemento
@@ -21,10 +30,10 @@ ordenarLista (x:xs) -- caso recursivo: x es cabeza y xs es cola
 main :: IO ()
 main = do
 -- pruebas de ordenar y filtrar lista
-   print (filtrarLista [1, 25, 5, -4] 0 5)
-   print (filtrarLista [10, 3, 7, 15, 2] 3 10)
-   print (filtrarLista [-5, -2, 0, 3, 8] (-3) 2)
-   print (filtrarLista [] 0 5 :: [Int])
+   print (filtrarLista [1, 25, 5, -4] 0 5)   -- Esperado: [1,5]
+   print (filtrarLista [10, 3, 7, 15, 2] 3 10)  -- Esperado: [10,3,7,2]
+   print (filtrarLista [-5, -2, 0, 3, 8] (-3) 2)  -- Esperado: [-2,0]
+   print (filtrarLista [] 0 5 :: [Int])  -- Esperado: []
    
    print (ordenarLista [1, 25, 5, -4])               -- esperado [25.0, 5.0, 1.0, -4.0]
    print (ordenarLista [10.5, 3.2, 7.8, 15.1, 2.9])  -- esperado: [ 15.1, 10.5,7.8, 3.2, 2.9]
