3044b6c36394958a2ee2c1547eb43381166676b3
matgimon18a-code <matgimon18a@gmail.com>
2025-08-24 20:05:13 -0500

    Main (2).hs

--- Main (2).hs
+++ Main (2).hs
@@ -1,9 +1,9 @@
-
+-- Coeficiente a(k)
 a :: Int -> Int -> Double
 a k n
     | k == 0    = sqrt (1 / fromIntegral n)
     | otherwise = sqrt (2 / fromIntegral n)
-
+-- DCT para un valor k
 dctK :: [Double] -> Int -> Double
 dctK xs k =
     let n = length xs
@@ -11,10 +11,10 @@ dctK xs k =
         terms = [ (xs !! n') * cos (((fromIntegral n' + 0.5) * pi * fromIntegral k) / fromIntegral n)
                 | n' <- [0..(n-1)] ]
     in coeff * sum terms
-
+-- DCT completa
 dct :: [Double] -> [Double]
 dct xs = [ dctK xs k | k <- [0 .. length xs - 1] ]
-
+--Este es el main principal donde se imprime la entrada y la salida del codigo, arroja el resultado.
 main :: IO ()
 main = do
     putStrLn "Transformada Discreta del Coseno (DCT)"
@@ -23,3 +23,4 @@ main = do
     print input
     putStrLn "Resultado DCT:"
     print (dct input)
+
