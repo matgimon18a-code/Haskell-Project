ead7de5dd4c4e6852f59c0c867141b4d2e6c0e99
matgimon18a-code <matgimon18a@gmail.com>
2025-08-24 20:03:54 -0500

    Main (3).hs

--- Main (3).hs
+++ Main (3).hs
@@ -1,28 +1,32 @@
+-- Funcion: filtrar lista
+-- Proposito: Elimina los elementos de una lista que estan fuera del intervalo dado
 filtrarLista :: (Num a, Ord a) => [a] -> a -> a -> [a]
-filtrarLista [] _ _ = []
-filtrarLista (x:xs) minimo maximo
-    | x >= minimo && x <= maximo = x : filtrarLista xs minimo maximo
-    | otherwise = filtrarLista xs minimo maximo
+filtrarLista [] _ _ = [] -- Caso base: lista vacia
+filtrarLista (x:xs) minimo maximo -- Caso recursivo: x es cabeza, xs es cola
+    | x >= minimo && x <= maximo = x : filtrarLista xs minimo maximo -- Si x esta en rango, incluirlo
+    | otherwise = filtrarLista xs minimo maximo --Si x esta fuera del rango, excluirlo
 
-
+--Funcion:ordenar lista
+--Proposito: Ordena una lista de numeros float en orden descendente(de mayor a menor.
 
 ordenarLista :: [Float] -> [Float]
-ordenarLista [] = []
-ordenarLista [x] = [x]
-ordenarLista (x:xs) 
-    | [] <- resto = [x]
-    | (y:ys) <- resto, x >= y = x : y : ys
-    | (y:ys) <- resto = y : ordenarLista (x : ys)
-    where resto = ordenarLista xs
+ordenarLista [] = [] -- caso base: lista vacia
+ordenarLista [x] = [x] -- caso base: un solo elemento
+ordenarLista (x:xs) -- caso recursivo: x es cabeza y xs es cola
+    | [] <- resto = [x] -- Si resto esta vacio, solo X
+    | (y:ys) <- resto, x >= y = x : y : ys -- Si x>= y, x va primero (orden descendente)
+    | (y:ys) <- resto = y : ordenarLista (x : ys) -- si x < y, y va primero, insertar x recursivamente 
+    where resto = ordenarLista xs -- primero ordenamos recursivamente 
 
 main :: IO ()
 main = do
+-- pruebas de ordenar y filtrar lista
    print (filtrarLista [1, 25, 5, -4] 0 5)
    print (filtrarLista [10, 3, 7, 15, 2] 3 10)
    print (filtrarLista [-5, -2, 0, 3, 8] (-3) 2)
    print (filtrarLista [] 0 5 :: [Int])
    
-   print (ordenarLista [1, 25, 5, -4])
-   print (ordenarLista [10.5, 3.2, 7.8, 15.1, 2.9])
-   print (ordenarLista [5.0])
-   print (ordenarLista [])
+   print (ordenarLista [1, 25, 5, -4])               -- esperado [25.0, 5.0, 1.0, -4.0]
+   print (ordenarLista [10.5, 3.2, 7.8, 15.1, 2.9])  -- esperado: [ 15.1, 10.5,7.8, 3.2, 2.9]
+   print (ordenarLista [5.0])                        -- esperado: [5.0]
+   print (ordenarLista [])                           -- esperado [] 
